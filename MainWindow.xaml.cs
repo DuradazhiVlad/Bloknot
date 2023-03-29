698d61b330aa0ef505c6543d7aae6a27e4daef1c
DuradazhiVlad <dvlad1995p@gmail.com>
2023-03-29 19:25:27 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Bloknot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string opendFile = "";
+        private string text;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //textBox.TextWrapping= TextWrapping.NoWrap;
+        }
+        public MainWindow(bool wrap , string text , int size)
+        {
+            InitializeComponent();
+            if (wrap)
+            {
+                textBox.TextWrapping = TextWrapping.Wrap;
+            }
+            else
+            {
+                textBox.TextWrapping = TextWrapping.NoWrap;
+            }
+            textBox.Text = text;
+            textBox.FontSize= size;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Files(*.*)|*.*|Text Files(*.txt)|*.txt";
+            openFileDialog.FilterIndex = 2;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                opendFile = openFileDialog.FileName;
+                using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                {
+                    textBox.Text = reader.ReadToEnd();
+                }
+            }
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "txt";
+            if (sfd.ShowDialog() == true)
+            {
+                opendFile = sfd.FileName;
+                //seveFile(sfd.FileName);
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1(textBox.Text);
+            window1.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bloknot
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string textTmp;
+        int fontSize;
+        public Window1(string text)
+        {
+            InitializeComponent();
+            checkBox.IsChecked = true;
+             textTmp=text;
+        }
+
+        
+
+        private void checkBox_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (checkBox.IsChecked == true)
+            {
+                checkBox.Content = "Обгортання тексту включено";                
+            }
+            if (checkBox.IsChecked == false)
+            {
+                checkBox.Content = "Обгортання тексту виключено";                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bool isWrapChecked = checkBox.IsChecked == true;
+            MainWindow mainWindow = new MainWindow(isWrapChecked, textTmp, fontSize);
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            fontSize = 5;
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            fontSize = 9;
+        }
+
+        private void RadioButton_Click_2(object sender, RoutedEventArgs e)
+        {
+            fontSize = 12;
+        }
+
+        private void RadioButton_Click_3(object sender, RoutedEventArgs e)
+        {
+            fontSize = 15;
+        }
+
+        private void RadioButton_Click_4(object sender, RoutedEventArgs e)
+        {
+            fontSize = 25;
+        }
+    }
+}
